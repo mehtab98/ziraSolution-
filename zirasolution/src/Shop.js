53bc17b68a41deeec8fb7fb49e96e531ebb675ee
Mehtab <mrandhawa@ucdavis.edu>
2020-07-08 12:29:22 -0700

    contact form

--- zirasolution/src/Shop.js
+++ zirasolution/src/Shop.js
@@ -1,34 +1,75 @@
 import React, {useState, useEffect} from 'react';
 import './css/App.css';
-import style from './css/genView.module.css';
-import CountUp from 'react-countup';
+import style from './css/form.module.css';
+
+import {Link} from 'react-router-dom'; 
+import { steelblue } from 'color-name';
+
 function Shop() {
-  useEffect(() => {
-  	
-  })
-  const fetchItems = async () =>{
-  	const data = await fetch('')
-  }
   return (
     <React.Fragment>
-  	  	<header className={style.header_views}>     
-      <img src={require("./images/it_network.jpeg")}  alt="fog over the bay"/>
-      <div class={style.hero_text_net}>
-        <h1>Contact US</h1>
-        <p> Tell us what your form of consultation you are looking for & we can soon be in contact.</p> 
-      </div>
+  	<header className={style.header_style}>
+      <h1 className={style.h1_form}>Contact Us</h1>
     </header>
 
+    <body className={style.BodyView}> 
+
+      <section className={style.section_style}>
+
+        <article className={style.article_view}>
+          <h1>Consultation Form</h1>
+          <form className={style.article_view}>
+          <h1>Name:</h1>
+          <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
+          <h1>Email:</h1>
+          <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
+          <h1>Phone number:</h1>
+          <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
 
 
+          <input type="submit" value="Submit" />
+          </form>
+        </article>
 
+      </section>
 
+    </body>
 
+    <footer class={style.footer_view}>
+      <div class={style.footer_row}>
+          
+          <div class={style.footer_column}> 
+            <h4 class={style.handle_text}>Cloud Management</h4>
+            <p class={style.handle_text}>2570 North First Street, Suite #200</p>
+            <p class={style.handle_text}> San Jose, CA 95131</p>
+            <p class={style.handle_text}> TEL: (650) 206-4009</p>
+            <p class={style.handle_text}> info@zirasolutions.com</p>
+          </div>
 
-    <footer className="footer_view">
-      <p>Footer</p>
+          <div class={style.footer_column}> 
+            <h4 class={style.handle_text}>Quick Links</h4>
+            <ul class={style.footer_links}>
+
+              <Link  to='/cloudMigration'>
+                <li class={style.link_stylez}>Contact US</li>
+              </Link>
+
+              <Link  to='/about'>
+                <li class={style.link_stylez}>About</li>
+              </Link>
+
+              <Link  to='/development'>
+                <li class={style.link_stylez}>Development</li>
+              </Link> 
+
+              <Link  to='/network'>
+                <li class={style.link_stylez}>Infrastructure</li>
+              </Link>
+
+            </ul>
+          </div>
+      </div> 
     </footer>
-
 
     </React.Fragment>
   );
